d1a0f4297bf221f9d9df5dffc9b11a5b7d023b7f
matchRos <Recker@match.uni-hannover.de>
2023-01-13 15:14:09 +0100

    added setup file

---
+++ setup.sh
@@ -0,0 +1,20 @@
+git submodule update --init --recursive
+cd voronoi_planner/nlopt/
+cmake ..
+make
+sudo make install
+
+cd ..
+
+cd tinyspline/
+mkdir build
+cd build
+cmake -DBUILD_SHARED_LIBS=True ..
+cmake --build .
+sudo cmake --build . --target install
+
+cd ../../..
+rosdep update
+rosdep install --from-paths src --ignore-src --rosdistro $ROS_DISTRO -y
+catkin build
+source devel/setup.bash
