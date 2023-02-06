14825b65ddf2553fa51b6a8137d047cbbba89757
Sönke <soenke.prophet@stud.uni-hannover.de>
2023-02-06 16:22:40 +0100

    revert last commit

--- splined_voronoi/splined_voronoi/src/splined_voronoi_planner.cpp
+++ splined_voronoi/splined_voronoi/src/splined_voronoi_planner.cpp
@@ -288,7 +288,7 @@ bool SplinedVoronoiPlanner::makePlan(const geometry_msgs::PoseStamped& start, co
     cv::Mat costmap_dist_img;
     cv::distanceTransform(~costmap_img_orig, costmap_dist_img, cv::DIST_L2, 3, CV_8UC1);
     cv::Mat costmap_img;
-    cv::threshold(costmap_dist_img, costmap_img, 1 / this->costmap_resolution_ / this->max_curvature_, 255, cv::THRESH_BINARY_INV);
+    cv::threshold(~costmap_img_orig, costmap_img, 1 / this->costmap_resolution_ / this->max_curvature_, 255, cv::THRESH_BINARY_INV);
     costmap_img.convertTo(costmap_img, CV_8UC1);
 
     this->obstacle_img_ = costmap_img;
